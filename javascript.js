f99f6332451cfa1eba2a1684fbd979872ca3b341
Cres1324 <krzysztof.Zawadzki.1324@gmail.com>
2022-08-08 13:00:38 +0200

    add ui to game

--- javascript.js
+++ javascript.js
@@ -1,25 +1,57 @@
 let choices = ["rock","paper","scissors"];
+let score = [0,0];
 function getComputerChoice(){
     choice=Math.floor(Math.random()*3);
     return choices[choice]
 };
 
 function playRound(compChoice,playerChoice){
+    playerChoice=playerChoice.toLowerCase();
+    console.log(playerChoice);
     if(compChoice == playerChoice){
-        playerChoice=playerChoice.toLowerCase();
     return "Draw, with "+compChoice;
     }
     else if(compChoice == "rock" && playerChoice=="scissors"  || compChoice == "scissors" && playerChoice == "paper" ||compChoice == "paper" && playerChoice =="rock"){
+        score[1]++;
         return "You lose, "+ compChoice+" beats "+playerChoice;
+
     }
     else{
-        return "You win, "+playerChoice+" beats" + compChoice;
+        score[0]++;
+        return "You win, "+playerChoice+" beats " + compChoice;
 }
 };
 
-function playGame(){
-    for(i=1;i<6;i++){
-        result=playRound(getComputerChoice(),prompt("It's time to choose"));
-        console.log(result);
-    };
-};
+document.addEventListener('click',playerChoosed)
+
+function playerChoosed(e) {
+    //document.querySelector(button.)
+    e.stopPropagation();
+    if(e.target.classList.contains('gameButton')){
+        const result = playRound(getComputerChoice(),e.target.textContent);
+        const resultText=document.querySelector('.result');
+        resultText.textContent=result;
+        const scoreText=document.querySelector('.score');
+        if(score[1]>=5){
+            resultText.textContent="Computer wins"
+            score=[0,0]
+        }
+        if(score[0]>=5){
+            resultText.textContent="You win"
+            score=[0,0]
+        }
+
+        scoreText.textContent=`Player score:${score[0]}  Computer score:${score[1]}`;
+    }  
+    else {
+        console.log('not today amigo')
+    } 
+
+}
+
+//function playGame(){
+ //   for(i=1;i<6;i++){
+  //      result=playRound(getComputerChoice(),prompt("It's time to choose"));
+  //      console.log(result);
+   // };
+///};*
